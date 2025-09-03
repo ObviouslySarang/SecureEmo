8eb7245ceee55235ad4f306f7382818e76cf2043
ObviouslySarang <2003krishnadixit@gmail.com>
2025-09-03 12:36:31 +0530

    revampimg the project

--- backend/src/utils/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
-const generateToken = (id, user) => {
-  return jwt.sign({ id: id, username: user.username }, "" + process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = generateToken;
--- frontend/src/Desktop/utils/appStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import passwordReducer from './passwordSlice.js'
-const appStore = configureStore({
-    reducer: {
-        passwords: passwordReducer
-    }
-})
-
-export default appStore;
